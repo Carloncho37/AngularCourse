3b22e566525daf0499a4e7fbea8e9dfce462ab8b
Carlos Galvan <carlos.galvan@ute-salta.com.ar>
2023-10-04 15:18:23 -0300

    add DBZ Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/components/heroes/heroes.module';
 //! Este import no se hace, ya que ahora es un modulo
-//!import { CounterComponent } from './counter/counter.component';
-//!import { HeroComponent } from './heroes/hero/hero.component';
-//!import { ListComponent } from './heroes/list/list.component';
+//! import { CounterComponent } from './counter/counter.component';
+//! import { HeroComponent } from './heroes/hero/hero.component';
+//! import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     //! Esta declaration ya no se hace, ahora es un modulo
     //! CounterComponent,
-    //!HeroComponent,
-    //!ListComponent
+    //! HeroComponent,
+    //! ListComponent
   ],
   imports: [
     BrowserModule,
     //* Se agrega aqui el modulo creado
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
